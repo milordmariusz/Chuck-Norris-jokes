3d88033cf93aa47ee46ad987ec15ed177e8facb0
milordmariusz <komodo454@wp.pl>
2022-07-26 20:46:26 +0200

    Change title screen
    Added styles to title and button

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,8 +1,11 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:chuck_norris_jokes_api/home/bloc/home_bloc.dart';
+import 'package:chuck_norris_jokes_api/home/home_colors.dart';
 import 'package:chuck_norris_jokes_api/services/chuck_joke_service.dart';
 import 'package:chuck_norris_jokes_api/services/connectivity_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -18,49 +21,135 @@ class HomePage extends StatelessWidget {
           body: BlocBuilder<HomeBloc, HomeState>(
             builder: (context, state) {
               if (state is HomeTitleState) {
-                return Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text("Chuck Norris jokes"),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      ElevatedButton(
-                          onPressed: () => BlocProvider.of<HomeBloc>(context)
-                              .add(LoadApiEvent()),
-                          child: Text("Load joke"))
-                    ],
+                return Container(
+                  decoration:
+                      const BoxDecoration(color: HomeColors.backgroundColor),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        TitleScreenText(
+                          content: 'Chuck Norris',
+                          size: 50.0,
+                        ),
+                        TitleScreenText(
+                          content: 'jokes',
+                          size: 50.0,
+                        ),
+                        SizedBox(
+                          height: 50,
+                        ),
+                        HomeButton(
+                          content: 'Start',
+                        )
+                      ],
+                    ),
                   ),
                 );
               }
               if (state is HomeLoadingState) {
-                return Center(
-                  child: CircularProgressIndicator(),
+                return Container(
+                  decoration:
+                      const BoxDecoration(color: HomeColors.backgroundColor),
+                  child: const Center(
+                    child: CircularProgressIndicator(),
+                  ),
                 );
               }
               if (state is HomeLoadedState) {
-                return Column(
-                  children: [
-                    Text(state.joke),
-                    Text(state.id.toString()),
-                    ElevatedButton(
-                        onPressed: () => BlocProvider.of<HomeBloc>(context)
-                            .add(LoadApiEvent()),
-                        child: Text("Next one")),
-                    ElevatedButton(
-                        onPressed: () => BlocProvider.of<HomeBloc>(context)
-                            .add(HomeTitleEvent()),
-                        child: Text("Return"))
-                  ],
+                return Container(
+                  decoration:
+                      const BoxDecoration(color: HomeColors.backgroundColor),
+                  child: Column(
+                    children: [
+                      Text(state.joke),
+                      Text(state.id.toString()),
+                      ElevatedButton(
+                          onPressed: () => BlocProvider.of<HomeBloc>(context)
+                              .add(LoadApiEvent()),
+                          child: const Text("Next one")),
+                      ElevatedButton(
+                          onPressed: () => BlocProvider.of<HomeBloc>(context)
+                              .add(HomeTitleEvent()),
+                          child: const Text("Return"))
+                    ],
+                  ),
                 );
               }
               if (state is HomeNoInternetState) {
-                return Text("no internet");
+                return Container(
+                    decoration:
+                        const BoxDecoration(color: HomeColors.backgroundColor),
+                    child: const Text("no internet"));
               }
               return Container();
             },
           ),
         ));
+  }
+}
+
+class HomeButton extends StatelessWidget {
+  const HomeButton({
+    Key? key,
+    required this.content,
+  }) : super(key: key);
+
+  final String content;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: ElevatedButton(
+        style: ButtonStyle(
+          padding: MaterialStateProperty.all(const EdgeInsets.all(20)),
+          minimumSize: MaterialStateProperty.all(Size(MediaQuery.of(context).size.width * 0.33, 40)),
+          side: MaterialStateProperty.all(
+            const BorderSide(
+              color: HomeColors.textWhiteColor,
+              width: 5,
+            ),
+          ),
+          shape: MaterialStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+          ),
+          backgroundColor: MaterialStateProperty.all(HomeColors.backgroundColor),
+          overlayColor: MaterialStateProperty.all(HomeColors.accentColor),
+        ),
+        onPressed: () => BlocProvider.of<HomeBloc>(context).add(LoadApiEvent()),
+        child: const TitleScreenText(
+          content: 'Start',
+          size: 25.0,
+        ),
+      ),
+    );
+  }
+}
+
+class TitleScreenText extends StatelessWidget {
+  const TitleScreenText({
+    Key? key,
+    required this.content,
+    required this.size,
+  }) : super(key: key);
+
+  final String content;
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    return AutoSizeText(
+      content,
+      style: GoogleFonts.poppins(
+        textStyle: TextStyle(
+          color: HomeColors.textWhiteColor,
+          fontSize: size,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      maxLines: 1,
+    );
   }
 }
---
+++ lib/home/home_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class HomeColors {
+  static const backgroundColor = Color(0xFF8FCB9B);
+  static const textWhiteColor = Color(0xFFEAE6E5);
+  static const textBlackColor = Color(0xFF12130F);
+  static const accentColor = Color(0xFF5B9279);
+  static const brownColor = Color(0xFF8F8073);
+}
